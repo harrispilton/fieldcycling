985a1786d7b7fbaa88fdf0c352227ec7a6dfd7f8
Mijo Wild <mijowild@gmail.com>
2017-04-21 19:38:28 +0200

    simplifies and refactors a bit
    A lot more of this is needed.
    I suspect the inconsistent usage of the variable x is responsible for some artefacts.
    Cleaning up modify_doc is essential.

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
-import sys
-from datetime import datetime
 import pandas as pd
 import numpy as np
-import re
-from bokeh.charts import Scatter, output_file, show
 from bokeh.models.widgets import Panel, Tabs
-from bokeh.io import output_file, show
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.palettes import Spectral5, viridis
 from bokeh.layouts import widgetbox
-from bokeh.models.widgets import Dropdown, Select, Button, DataTable, TableColumn, Slider, RangeSlider, TextInput#, ColumnDataSource
+from bokeh.models.widgets import Select, Button, DataTable, TableColumn, Slider, RangeSlider, TextInput#, ColumnDataSource
 from bokeh.models.callbacks import CustomJS
 from tornado.ioloop import IOLoop
 from bokeh.application.handlers import FunctionHandler
@@ -21,31 +16,25 @@ from bokeh.layouts import column, row
 #from bokeh.models import ColumnDataSource#, Slider, RangeSlider, TextInput
 from bokeh.server.server import Server
 import stelardatafile as sdf
-from utils import get_x_axis, model_exp_dec, fun_exp_dec, get_mag_amplitude, magnetization_fit
+from utils import model_exp_dec, fun_exp_dec, get_mag_amplitude, magnetization_fit
 from scipy.optimize import leastsq
-
-#specify and import data file
-path=os.path.join(os.path.curdir,'data')
-polymer=sdf.StelarDataFile('glyzerin_d3_300K.sdf',path)
-polymer.sdfimport()
-nr_experiments = polymer.get_number_of_experiments()
-polymer.rephase_fids()
-
-# parameters to dataframe
-par_df, columns, discrete, continuous, time, quantileable = polymer.scan_parameters(20)
-
-
-#initially set experiment number ie=1
-ie = 1
 
 #TODO: work with more than one tab in the browser: file input, multiple data analysis and manipulation tabs, file output and recent results tab
 #TODO: clean modify_doc, it is awfully crowded here...
 def modify_doc(doc):
+
+    #load data:
+    polymer = load_data()
+    # initialy set ie = 1
+    ie = 1
+    # parameters to dataframe
+    par_df, columns, discrete, continuous, time, quantileable = polymer.scan_parameters(20)
+
+
     fid=polymer.getfid(ie) #fid is a dataframe containing fids of experiment ie
     rephased_fid=polymer.getparvalue(ie,'rephased_fid')
 
-    # TODO: more testing on get_x_axis
-    tau = get_x_axis(polymer.getparameter(ie))
+    tau = polymer.get_tau_axis(ie)
 
     #calculate magnetization:
     startpoint=int(0.05*polymer.getparvalue(ie,'BS'))
@@ -59,30 +48,24 @@ def modify_doc(doc):
     df = pd.DataFrame(data=np.c_[tau, phi], columns=['tau', 'phi'])
     df['phi_normalized']=(df['phi'] - df['phi'].iloc[0] ) / (df['phi'].iloc[-1] - df['phi'].iloc[1] )
     polymer.addparameter(ie,'df_magnetization',df)
-    
 
-    
     fit_option = 2
     p0 = [1 , 2 * polymer.getparvalue(ie,'T1MX')**-1, 0]
     df, popt = magnetization_fit(df, p0, fit_option)
     polymer.addparameter(ie,'popt(mono_exp)',popt)
     
     df['fit_phi'] = model_exp_dec(df.tau, *popt)
-    
 
-    
     # create and plot figures
     p1 = figure(plot_width=800, plot_height=500,
                 title='Free Induction Decay', webgl=True,
                 lod_factor=1000,lod_interval=150,lod_threshold=1000)
-
 
     # convert data to handle in bokeh
     source_fid = ColumnDataSource(data=ColumnDataSource.from_df(rephased_fid))
     p1.line('index', 'im', source=source_fid, color='blue')
     p1.line('index', 'real', source=source_fid, color='green')
     p1.line('index', 'magnitude', source=source_fid, color='red')
-    
 
     source_mag_dec = ColumnDataSource(data=ColumnDataSource.from_df(df))
     p2 = figure(plot_width=300, plot_height=300,
@@ -90,13 +73,13 @@ def modify_doc(doc):
     p2.circle_cross('tau', 'phi_normalized', source=source_mag_dec, color="navy")
     p2.line('tau', 'fit_phi', source=source_mag_dec, color="teal")
 
-    
     # in the plot 4 use followingimpo
     SIZES = list(range(6, 22, 3)) # for some sizes
     COLORS = Spectral5 # for some colors (more colors would be nice somehow)
 
     def plot_par():
-        xs = par_df[x.value].values
+        ''' Creates plot for the parameters '''
+        xs = par_df[x.value ].values
         ys = par_df[y.value].values
         x_title = x.value.title()
         y_title = y.value.title()
@@ -120,10 +103,9 @@ def modify_doc(doc):
         p4.xaxis.axis_label = x_title
         p4.yaxis.axis_label = y_title
 
-
         if x.value in discrete:
             p4.xaxis.major_label_orientation = pd.np.pi / 4 # rotates labels...
-                    
+
         sz = 9
         if size.value != 'None':
             groups = pd.qcut(pd.to_numeric(par_df[size.value].values), len(SIZES))
@@ -138,8 +120,8 @@ def modify_doc(doc):
         return p4
     
     def update(attr, old, new):
-        tabs.tabs[1].child.children[1] = plot_par() #update figure 1 in parameters tab
-        #layout_p4.children[1] = plot_par()
+        ''' updates figure 1 in parameters tab '''
+        tabs.tabs[1].child.children[1] = plot_par()
 
     def experiment_update(attr, old, new):
         ie = experiment_slider.value
@@ -158,7 +140,8 @@ def modify_doc(doc):
         fid = polymer.getfid(ie) #read FID or series of FIDs for selected experiment
         source_fid.data=ColumnDataSource.from_df(fid) #convert fid to bokeh format
         try:
-            tau = get_x_axis(polymer.getparameter(ie)) #numpy array containing the taus for experiment ie
+            # TODO: merge this with the initial plot. Now its doubled in the code
+            tau = polymer.get_tau_axis(ie) #numpy array containing the taus for experiment ie
             startpoint=fid_slider.range[0] #lower integration bound
             endpoint = fid_slider.range[1] #upper integration bound
             polymer.addparameter(ie,'fid_range',(startpoint,endpoint)) #add integration range to parameters to make it accesible
@@ -185,36 +168,37 @@ def modify_doc(doc):
             df['fit_phi'] = np.zeros(1)
             source_mag_dec.data = ColumnDataSource.from_df(df)
 
-    
+    nr_experiments = polymer.get_number_of_experiments()
+    # initiate slider to choose experiment
     experiment_slider = Slider(start=1, end=nr_experiments, value=1, step=1,callback_policy='mouseup') #select experiment by value
+    # initiate slider for the range in which fid shall be calculated
     fid_slider = RangeSlider(start=1,end=polymer.getparvalue(ie,'BS'),range=polymer.getparvalue(ie,'fid_range'),step=1,callback_policy='mouseup')#select the intervall from which magneitization is calculated from fid
-
 
     source = ColumnDataSource(data=dict(value=[]))
     source.on_change('data',experiment_update) #source for experiment_slider
     source2 = ColumnDataSource(data=dict(range=[], ie=[]))
     source2.on_change('data',calculate_mag_dec)#source for fid_slider
 
-
     experiment_slider.callback = CustomJS(args=dict(source=source),code="""
         source.data = { value: [cb_obj.value] }
     """)#unfortunately this customjs is needed to throttle the callback in current version of bokeh
-    
+
     fid_slider.callback=CustomJS(args=dict(source=source2),code="""
         source.data = { range: cb_obj.range }
     """)#unfortunately this customjs is needed to throttle the callback in current version of bokeh
-    
-    
+
     def update_parameters():
+        ''' updates parameters of polymer, since they can change during evaluation '''
         par_df, columns, discrete, continuous, time, quantileable = polymer.scan_parameters()
         x.options=columns
         y.options=columns
         size.options=['None']+quantileable
         color.options=['None']+quantileable
+
     # select boxes for p4
     button_scan = Button(label='Scan Parameters',button_type="success")
     button_scan.on_click(update_parameters)
-    
+
     x = Select(title='X-Axis', value='ZONE', options=columns)
     x.on_change('value', update)
 
@@ -227,28 +211,26 @@ def modify_doc(doc):
     color = Select(title='Color', value='None', options=['None'] + quantileable)
     color.on_change('value', update)
 
-
-
-
     controls_p4 = widgetbox([button_scan, x,y,color,size], width=150)
     layout_p4 = row(controls_p4,plot_par())
 
-    
     #fitting on all experiments
     p3 = figure(plot_width=300, plot_height=300,
             title='normalized phi vs normalized tau', webgl = True,
                 y_axis_type = 'log',
                 x_axis_type = 'linear')
-    
+
     #fit magnetization decay for all experiments
+    nr_experiments = polymer.get_number_of_experiments()
     r1=np.zeros(nr_experiments)
     MANY_COLORS = 0
     p3_line_glyph=[]
-    for i in range(1,nr_experiments):
+    for i in range(1, nr_experiments):
         try:
             par=polymer.getparameter(i)
             fid=polymer.getfid(i)
-            tau= get_x_axis(polymer.getparameter(i))
+            # TODO: here tau axis is being calculated the 3rd time. Look if it can be simplified
+            tau= polymer.get_tau_axis(i)
             try:
                 startpoint=polymer.getparvalue(i,'fid_range')[0]
                 endpoint=polymer.getparvalue(i,'fid_range')[1]
@@ -284,13 +266,12 @@ def modify_doc(doc):
         p3_line_glyph[ic].glyph.line_color=COLORS[ic]
     par_df['r1']=r1
 
-
     ####
     #### TODO: write file input
     #### TODO: select files to import
     #### TODO: discard imported files
     ####
-    
+
     table_source=ColumnDataSource(data=dict())
     sdf_list=[polymer]
     filenames=[x.file() for x in sdf_list]
@@ -306,8 +287,9 @@ def modify_doc(doc):
     filebox=TextInput(title="Filename",value="*.sdf")
 
     layout_input=column(pathbox,filebox,table)  
-    
+
     def table_update(attr,old,new):
+        ''' loads data '''
         print('Hello table')
         path=pathbox.value.strip()
         file=filebox.value.strip()
@@ -326,29 +308,35 @@ def modify_doc(doc):
     pathbox.on_change('value',table_update)
     filebox.on_change('value',table_update)
 
-        
     layout_p1 = column(experiment_slider, p1,fid_slider, p2, p3)
     tab_relaxation = Panel(child = layout_p1, title = 'Relaxation')
     tab_parameters = Panel(child = layout_p4, title = 'Parameters')
     tab_input = Panel(child = layout_input, title = 'Data In')
     tabs = Tabs(tabs = [tab_relaxation, tab_parameters, tab_input])
 
-
-    
     doc.add_root(tabs)
     doc.add_root(source) # i need to add source to detect changes
     doc.add_root(source2)
 
+def load_data():
+    #specify and import data file
+    path=os.path.join(os.path.curdir,'data')
+    # initialize StelarDataFile Object
+    polymer=sdf.StelarDataFile('glyzerin_d3_300K.sdf',path)
+    polymer.sdfimport()
+    nr_experiments = polymer.get_number_of_experiments()
+    polymer.rephase_fids()
+    return polymer
 
-
-bokeh_app = Application(FunctionHandler(modify_doc))
-
-io_loop = IOLoop.current()
-server = Server({'/': bokeh_app}, io_loop=io_loop)
-server.start()
+def main():
+    bokeh_app = Application(FunctionHandler(modify_doc))
+    io_loop = IOLoop.current()
+    print('Opening Bokeh application on http://localhost:5006/')
+    server = Server({'/': bokeh_app}, io_loop=io_loop)
+    server.start()
+    io_loop.add_callback(server.show, "/")
+    io_loop.start()
 
 if __name__ == '__main__':
-    print('Opening Bokeh application on http://localhost:5006/')
+    main()
 
-    io_loop.add_callback(server.show, "/")
-io_loop.start()
--- stelardatafile.py
+++ stelardatafile.py
@@ -2,8 +2,11 @@ import os
 import sys
 import re
 import numpy as np
+import numpy.matlib
 import pandas as pd
+from num_string_eval import NumericStringParser
 from utils import freq_shift
+
 
 class StelarDataFile:
     def __init__(self, FileName, PathName):
@@ -56,6 +59,49 @@ class StelarDataFile:
 
     def get_number_of_experiments(self):
         return len(self.datas)
+    
+    def get_tau_axis(self, ie):
+        parameters = self.getparameter(ie)
+        nsp = NumericStringParser()
+        nblk = parameters['NBLK']
+        T1MX = parameters['T1MX'] # T1MX is used in the 'eval' expressions below
+        # TODO: not tested yet for all cases
+        if parameters['BGRD'] == 'LIST':
+            print('BGRD = LIST')
+            temp = parameters['BLST']
+            temp.replace(';', ':')
+            sep_indices = [pos for pos, char in enumerate(temp) if char == ':'] # find indices of ':'
+            Tini = nsp.eval(temp[:sep_indices[0]].replace('T1MX', str(T1MX)))
+            Tend = nsp.eval(temp[sep_indices[0]+1:sep_indices[1]].replace('T1MX', str(T1MX)))
+            npts = nsp.eval(temp[sep_indices[2]+1:].replace('T1MX', str(T1MX))) # number of points selected: can be ~= NBLK    
+            if temp[sep_indices[1]+1:sep_indices[2]] == 'LIN':
+                listx = np.linspace(Tini,Tend,npts);
+            elif temp[sep_indices[1]+1:sep_indices[2]] == 'LOG':
+                listx = np.logspace(np.log10(Tini),np.log10(Tend),npts);
+            nrep = np.ceil(nblk/npts) # find if the time vector needs to be longer
+            x = numpy.matlib.repmat(listx,1,nrep) # re-create the time vector
+            x = x[:nblk] # select the portion corresponding to the number of blocs (needed if npts~=nblk)
+        elif parameters['BGRD'] == 'LIN':
+            print('BGRD = LIN')
+            Tini = nsp.eval(parameters['BINI'].replace('T1MX', str(T1MX)))
+            Tend = nsp.eval(parameters['BEND'].replace('T1MX', str(T1MX)))
+            x = np.linspace(Tini, Tend, nblk) # re-create the time vector
+        elif parameters['BGRD'] == 'LOG':
+            print('BGRD = LOG')
+            Tini_Tend = [0, 0]
+            # This has to be so complicated b/c of mixed datatype that is possible for parameters['BGRD'].
+            # Documentation would be beneficial, what can occur
+            for nb, b in enumerate(['BINI', 'BEND']):
+                if type(parameters[b]) == type(0.0):
+                    Tini_Tend[nb] = parameters[b]
+                elif type(parameters[b]) == type('asdf'):
+                    if 'T1MX' in parameters[b]:
+                        Tini_Tend[nb] = nsp.eval(parameters[b].replace('T1MX', str(T1MX)))
+                    else:
+                        Tini_Tend[nb] = nsp.eval(parameters[b])
+            Tini, Tend = Tini_Tend
+            x = np.logspace(np.log10(Tini),np.log10(Tend),nblk) # re-create the time vector
+        return x
 
     def addparameter(self, ie, par, val):
         parameter, data = self.datas[ie]
@@ -127,17 +173,15 @@ class StelarDataFile:
                             parameters['TIME']=pd.to_datetime(words[1])
                         except:
                             print('TIME is not in datetime Format')
-                        
-
                 try:
-                     if 'DATA' in words[0]:
+                    if 'DATA' in words[0]:
                         data=[]
                         if parameters['NBLK']==0:
                             parameters.update({'NBLK': 1})
                         for i in range(0,int(parameters['NBLK']*parameters['BS'])):
                             columns=fid.readline().replace('\n','').split('\t')
                             data.append([])
-                            for c, ii in zip(columns, range(0,10)):
+                            for c in columns:
                                 data[i].append(int(c))
                         self.adddata(ie,parameters,data)
                         #print(self.getexp(ie))
